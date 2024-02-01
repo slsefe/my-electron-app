cde4f8c4fc98946ac7f8d1018a72be2de6a3fc17
slsefe <17693374267@163.com>
2024-02-01 21:38:43 +0800

    add publish.

--- forge.config.js
+++ forge.config.js
@@ -27,4 +27,17 @@ module.exports = {
       config: {},
     },
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'slsefe',
+          name: 'my-electron-app'
+        },
+        prerelease: false,
+        draft: true
+      }
+    }
+  ],
 };
